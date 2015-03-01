a343b401b83183a8f2dfbb72a12d6e06016a0821
Jose Araujo <arjo1203@stcloudstate.edu>
2015-02-28 21:36:42 -0600

    Added all apps

---
+++ multiDraw/multi-touch.js
@@ -0,0 +1,63 @@
+paper.install(window);
+
+var paths = new Array();
+
+
+startup();
+            
+function startup(){
+    //PaperJS Installation
+    paper.setup('canvas');
+    paper.view.viewSize = [window.innerWidth, window.innerHeight];
+    
+    var soilMap = new Raster('img');
+    soilMap.position = paper.view.center;
+    console.log(paper.view);
+    
+    document.body.addEventListener('touchstart', touchStart, false);
+    document.body.addEventListener('touchmove', touchmove, false);
+    document.body.addEventListener('touchend', touchEnd, false);
+}
+
+
+
+//Array to store new paths
+var paths = [];
+
+function touchStart(ev){
+    var touches = ev.touches;
+
+    //Create new path per touch
+    var path = new Path();
+    path.strokeColor = 'red';
+    path.strokeWidth = 4;
+    paths.push(path);
+}
+
+
+function touchmove(ev) {  
+    var touches = ev.touches;
+
+    //Prevents touch bubbling 
+    if(touches.length == paths.length){
+        for(var i = 0; i < touches.length; i++){
+            var path = paths[i];
+            var touch = touches[i];
+            var x1, y1;
+            x1 = touch.pageX;
+            y1 = touch.pageY;
+            path.add(new Point(x1, y1));
+            paper.view.draw();  
+        }
+    } 
+}
+
+
+function touchEnd(ev){   
+    var touches = ev.touches
+
+    //Empth paths array to start process over
+    if(touches.length == 0){
+        paths.splice(0, paths.length);
+    }
+}
