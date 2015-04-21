f45fa267663d402745b778409de215c80c54dee5
Jose Araujo <arjo1203@stcloudstate.edu>
2015-04-20 22:33:20 -0500

    added carousel

--- quadraticSurfaces/quadraticSurfaces.js
+++ quadraticSurfaces/quadraticSurfaces.js
@@ -1,42 +1,5 @@
-$("#planeTilt").slider();
-$("#planeTilt").on('slide', function(slideEvt) {
-    $("#planeTiltVal").text(slideEvt.value);
-    PLANE.rotation.x = degInRad(slideEvt.value);
-    render();
-});
-
-$("#planePos").slider();
-$("#planePos").on('slide', function(slideEvt) {
-    $("#planePosVal").text(slideEvt.value);
-    PLANE.position.z = slideEvt.value;
-    render();
-});
-
-$("#xScale").slider();
-$("#xScale").on('slide', function(slideEvt) {
-    $("#xScaleVal").text(slideEvt.value);
-    if(slideEvt.value != 0){
-        GRAPH.scale.x = 1 / Math.pow(slideEvt.value, 2);
-        render();
-    }
-});
-
-$("#yScale").slider();
-$("#yScale").on('slide', function(slideEvt) {
-    $("#yScaleVal").text(slideEvt.value);
-    if(slideEvt.value != 0){
-        GRAPH.scale.y = 1 / Math.pow(slideEvt.value, 2);
-        render();
-    }
-});
-
 //Global variables for THREE environment
-var camera, scene, renderer, controls;
-var PLANE, GRAPH;
-var menu_toggle = $("#menu-toggle"), onOffCounter = 0, planeOnOff = 0, planeOnOffButton = $('#planeOnOff');
-var Hyperboloid1Button = $('#Hyperboloid1'), Hyperboloid2Button = $('#Hyperboloid2'), ConeButton = $('#Cone'),
-    EllipticParaboloidButton = $('#EllipticParaboloid'), HyperbolicParaboloidButton = $('#HyperbolicParaboloid'),
-    graphName = $('#graphName'), graphFn = $('#graphFn');
+var camera, scene, renderer, controls, PLANE, GRAPH;
 
 
 
@@ -54,14 +17,13 @@ function init(onSuccess){
     scene = new THREE.Scene();
 
     //Creates camera and sets position in the THREE environment, this allow use to view the THREE enviroment
-    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000000000);
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 100000);
     camera.position.set(0, -400, 100);
     camera.lookAt(scene.position);
 
     //Creates renderer for THREE environment, sets the size of the renderer, and appends the renderer to html document
     renderer = new THREE.WebGLRenderer({canvas: threejs});
     renderer.setSize(window.innerWidth, window.innerHeight);
-    //document.body.appendChild(renderer.domElement);
 
     //Adds mouse/touch controls to the camera
     controls = new THREE.TrackballControls(camera, renderer.domElement);
@@ -69,7 +31,6 @@ function init(onSuccess){
     controls.addEventListener('change', render);
 
     GRAPH = makeHyperbolicParaboloid();
-    GRAPH.id = 'HyperbolicParaboloid';
     scene.add(GRAPH);
 
     //Assigns the 'resize' eventListener to call the onWindowResize function whenever the window's size has changed
@@ -81,6 +42,7 @@ function init(onSuccess){
     //Called when init has completed
     onSuccess();
 }
+
 
 
 
@@ -97,12 +59,14 @@ function makePlane(){
 
 
 
+
 function makeHyperbolicParaboloid(){
     var x, y, xMin = -10, yMin = -10, xRange = 20, yRange = 20, segments = 40;
     var zFunc = function(a, b){
         var result = Math.pow(a, 2) - Math.pow(b, 2);
         return result;
     };
+
     meshFunction = function(u, v)
     {
         x = xRange * u + xMin;
@@ -122,6 +86,7 @@ function makeHyperbolicParaboloid(){
 
     return graphMesh;
 }
+
 
 
 
@@ -131,6 +96,7 @@ function makeEllipticParaboloid(){
         var result = Math.pow(a, 2) + Math.pow(b, 2);
         return result;
     };
+
     meshFunction = function(u, v)
     {
         x = xRange * u + xMin;
@@ -153,13 +119,14 @@ function makeEllipticParaboloid(){
 
 
 
+
 function makeCone(){
-    var cone = new THREE.Object3D();
-    var x, y, xMin = -10, yMin = -10, xRange = 20, yRange = 20, segments = 50;
+    var cone = new THREE.Object3D(), x, y, xMin = -20, yMin = -20, xRange = 40, yRange = 40, segments = 40;
     var zFunc = function(a, b){
         var result = Math.pow(Math.pow(a, 2) + Math.pow(b, 2), .5);
         return result;
     };
+
     meshFunction = function(u, v)
     {
         x = xRange * u + xMin;
@@ -182,6 +149,7 @@ function makeCone(){
         var result = -Math.pow(Math.pow(a, 2) + Math.pow(b, 2), .5);
         return result;
     };
+
     meshFunction2 = function(u, v)
     {
         x = xRange * u + xMin;
@@ -205,13 +173,14 @@ function makeCone(){
 
 
 
+
 function makeHyperboloid1(){
-    var hyperboloid = new THREE.Object3D();
-    var x, y, xMin = -10, yMin = -10, xRange = 20, yRange = 20, segments = 40;
+    var hyperboloid = new THREE.Object3D(), x, y, xMin = -20, yMin = -20, xRange = 40, yRange = 40, segments = 40;
     var zFunc = function(a, b){
         var result = Math.pow(Math.pow(a, 2) + Math.pow(b, 2) - 1, .5);
         return result;
     };
+
     meshFunction = function(u, v)
     {
         x = xRange * u + xMin;
@@ -234,6 +203,7 @@ function makeHyperboloid1(){
         var result = -Math.pow(Math.pow(a, 2) + Math.pow(b, 2) - 1, .5);
         return result;
     };
+
     meshFunction2 = function(u, v)
     {
         x = xRange * u + xMin;
@@ -257,13 +227,14 @@ function makeHyperboloid1(){
 
 
 
+
 function makeHyperboloid2(){
-    var hyperboloid = new THREE.Object3D();
-    var x, y, xMin = -10, yMin = -10, xRange = 20, yRange = 20, segments = 40;
+    var hyperboloid = new THREE.Object3D(), x, y, xMin = -20, yMin = -20, xRange = 40, yRange = 40, segments = 40;
     var zFunc = function(a, b){
         var result = Math.pow(Math.pow(a, 2) + Math.pow(b, 2) + 1, .5);
         return result;
     };
+
     meshFunction = function(u, v)
     {
         x = xRange * u + xMin;
@@ -286,6 +257,7 @@ function makeHyperboloid2(){
         var result = -Math.pow(Math.pow(a, 2) + Math.pow(b, 2) + 1, .5);
         return result;
     };
+
     meshFunction2 = function(u, v)
     {
         x = xRange * u + xMin;
@@ -309,13 +281,14 @@ function makeHyperboloid2(){
 
 
 
+
 function makeEllipsoid(){
-    var ellipsoid = new THREE.Object3D();
-    var x, y, xMin = -10, yMin = -10, xRange = 20, yRange = 20, segments = 40;
+    var ellipsoid = new THREE.Object3D(), x, y, xMin = -10, yMin = -10, xRange = 20, yRange = 20, segments = 40;
     var zFunc = function(a, b){
         var result = Math.pow(-Math.pow(a, 2) - Math.pow(b, 2), .5);
         return result;
     };
+
     meshFunction = function(u, v)
     {
         x = xRange * u + xMin;
@@ -338,6 +311,7 @@ function makeEllipsoid(){
         var result = -Math.pow(-Math.pow(a, 2) - Math.pow(b, 2), .5);
         return result;
     };
+
     meshFunction2 = function(u, v)
     {
         x = xRange * u + xMin;
@@ -361,112 +335,12 @@ function makeEllipsoid(){
 
 
 
+
 function degInRad(deg) {
     return deg * Math.PI / 180;
 }
 
 
-
-HyperbolicParaboloidButton.click(function() {
-    graphName[0].innerHTML = 'Hyperbolic Paraboloid';
-    graphFn[0].innerHTML = 'z = (x^2/a^2) - (y^2/b^2)';
-
-    scene.remove(GRAPH);
-
-    GRAPH = makeHyperbolicParaboloid();
-    scene.add(GRAPH);
-
-    render();
-});
-
-
-
-ConeButton.click(function() {
-    graphName[0].innerHTML = 'Cone';
-    graphFn[0].innerHTML = 'z^2 = (x^2/a^2) + (y^2/b^2)';
-
-    scene.remove(GRAPH);
-
-    GRAPH = makeCone();
-    scene.add(GRAPH);
-
-    render();
-});
-
-
-
-
-Hyperboloid1Button.click(function() {
-    graphName[0].innerHTML = 'Hyperboloid 1 Sheet';
-    graphFn[0].innerHTML = '(x^2/a^2) + (y^2/b^2) - z^2 = 1';
-
-    scene.remove(GRAPH);
-
-    GRAPH = makeHyperboloid1();
-    scene.add(GRAPH);
-
-    render();
-});
-
-
-
-
-Hyperboloid2Button.click(function() {
-    graphName[0].innerHTML = 'Hyperboloid 2 Sheet';
-    graphFn[0].innerHTML = '-(x^2/a^2) - (y^2/b^2) + z^2 = 1';
-
-    scene.remove(GRAPH);
-
-    GRAPH = makeHyperboloid2();
-    scene.add(GRAPH);
-
-    render();
-});
-
-
-
-
-planeOnOffButton.click(function() {
-    if(planeOnOff % 2 == 1){
-        scene.remove(PLANE);
-        render();
-        planeOnOffButton[0].innerHTML = 'Turn Plane On';
-    }
-    else if(planeOnOff % 2 == 0){
-        PLANE = makePlane();
-        scene.add(PLANE);
-        render()
-        planeOnOffButton[0].innerHTML = 'Turn Plane Off';
-    }
-    planeOnOff++;
-});
-
-
-EllipticParaboloidButton.click(function() {
-    graphName[0].innerHTML = 'Elliptic Paraboloid';
-    graphFn[0].innerHTML = 'z = (x^2/a^2) + (y^2/b^2)';
-
-    scene.remove(GRAPH);
-
-    GRAPH = makeEllipticParaboloid();
-    scene.add(GRAPH);
-
-    render();
-});
-
-
-
-
-menu_toggle.click(function() {
-    if(onOffCounter % 2 == 0){
-        menu_toggle[0].innerHTML = 'Open';//change label of button when clicked
-    }
-    else{
-        menu_toggle[0].innerHTML = 'Close';
-    }
-
-    onOffCounter++;
-});
 
 
 function onWindowResize(){
@@ -480,15 +354,22 @@ function onWindowResize(){
 
 
 
-var i = 0;
+var animateaGARPH = false;
 function animate(){
+    //animateaGARPH = flag || false;
+    //console.log(animateaGARPH);
+    var rand1 = 4, rand2 = 4;
+
     //Called every frame
     requestAnimationFrame(animate);
 
-    //PLANE.position.z += -1;
-
     //Update the controls for the camera
     controls.update();
+
+    if(animateaGARPH){
+        GRAPH.scale.x += (1 / Math.pow(rand1, 2));
+        GRAPH.scale.y += (1 / Math.pow(rand1, 2));
+    }
 
     //Renders the THREE environment
     render();
---
+++ quadraticSurfaces/slidersAndButtons.js
@@ -0,0 +1,185 @@
+//Buttons
+var menu_toggle = $("#menu-toggle"), planeOnOffButton = $('#planeOnOff'), Hyperboloid1Button = $('#Hyperboloid1'),
+    Hyperboloid2Button = $('#Hyperboloid2'), ConeButton = $('#Cone'), EllipticParaboloidButton = $('#EllipticParaboloid'),
+    HyperbolicParaboloidButton = $('#HyperbolicParaboloid'), animateButton = $('#animate');
+
+//Divs
+var graphName = $('#graphName'), graphFn = $('#graphFn');
+
+
+
+$("#planeTilt").slider();
+$("#planeTilt").on('slide', function(slideEvt) {
+    $("#planeTiltVal").text(slideEvt.value);
+    PLANE.rotation.x = degInRad(slideEvt.value);
+    render();
+});
+
+$("#planePos").slider();
+$("#planePos").on('slide', function(slideEvt) {
+    $("#planePosVal").text(slideEvt.value);
+    PLANE.position.z = slideEvt.value;
+    render();
+});
+
+$("#xScale").slider();
+$("#xScale").on('slide', function(slideEvt) {
+    $("#xScaleVal").text(slideEvt.value);
+    if(slideEvt.value != 0){
+        GRAPH.scale.x = 1 / Math.pow(slideEvt.value, 2);
+        render();
+    }
+});
+
+$("#yScale").slider();
+$("#yScale").on('slide', function(slideEvt) {
+    $("#yScaleVal").text(slideEvt.value);
+    if(slideEvt.value != 0){
+        GRAPH.scale.y = 1 / Math.pow(slideEvt.value, 2);
+        render();
+    }
+});
+
+
+
+
+HyperbolicParaboloidButton.click(function() {
+    var xScale = GRAPH.scale.x, yScale = GRAPH.scale.y;
+
+    graphName[0].innerHTML = 'Hyperbolic Paraboloid';
+    graphFn[0].innerHTML = 'z = (x^2/a^2) - (y^2/b^2)';
+
+    scene.remove(GRAPH);
+
+    GRAPH = makeHyperbolicParaboloid();
+    GRAPH.scale.x = xScale;
+    GRAPH.scale.y = yScale;
+    scene.add(GRAPH);
+
+    render();
+});
+
+
+
+
+ConeButton.click(function() {
+    var xScale = GRAPH.scale.x, yScale = GRAPH.scale.y;
+
+    graphName[0].innerHTML = 'Cone';
+    graphFn[0].innerHTML = 'z^2 = (x^2/a^2) + (y^2/b^2)';
+
+
+    scene.remove(GRAPH);
+
+    GRAPH = makeCone();
+    GRAPH.scale.x = xScale;
+    GRAPH.scale.y = yScale;
+    scene.add(GRAPH);
+
+    render();
+});
+
+
+
+
+Hyperboloid1Button.click(function() {
+    var xScale = GRAPH.scale.x, yScale = GRAPH.scale.y;
+
+    graphName[0].innerHTML = 'Hyperboloid 1 Sheet';
+    graphFn[0].innerHTML = '(x^2/a^2) + (y^2/b^2) - z^2 = 1';
+
+    scene.remove(GRAPH);
+
+    GRAPH = makeHyperboloid1();
+    GRAPH.scale.x = xScale;
+    GRAPH.scale.y = yScale;
+    scene.add(GRAPH);
+
+    render();
+});
+
+
+
+
+Hyperboloid2Button.click(function() {
+    var xScale = GRAPH.scale.x, yScale = GRAPH.scale.y;
+
+    graphName[0].innerHTML = 'Hyperboloid 2 Sheet';
+    graphFn[0].innerHTML = '-(x^2/a^2) - (y^2/b^2) + z^2 = 1';
+
+    scene.remove(GRAPH);
+
+    GRAPH = makeHyperboloid2();
+    GRAPH.scale.x = xScale;
+    GRAPH.scale.y = yScale;
+    scene.add(GRAPH);
+
+    render();
+});
+
+
+
+
+EllipticParaboloidButton.click(function() {
+    var xScale = GRAPH.scale.x, yScale = GRAPH.scale.y;
+
+    graphName[0].innerHTML = 'Elliptic Paraboloid';
+    graphFn[0].innerHTML = 'z = (x^2/a^2) + (y^2/b^2)';
+
+    scene.remove(GRAPH);
+
+    GRAPH = makeEllipticParaboloid();
+    GRAPH.scale.x = xScale;
+    GRAPH.scale.y = yScale;
+    scene.add(GRAPH);
+
+    render();
+});
+
+
+
+
+planeOnOffButton.click(function() {
+    var currentState = planeOnOffButton[0].innerHTML;
+
+    if(currentState == 'Turn Plane On') {
+        PLANE = makePlane();
+        scene.add(PLANE);
+        render();
+        planeOnOffButton[0].innerHTML = 'Turn Plane Off';
+    }
+    else {
+        scene.remove(PLANE);
+        render();
+        planeOnOffButton[0].innerHTML = 'Turn Plane On';
+    }
+});
+
+
+
+
+menu_toggle.click(function() {
+    var currentState = menu_toggle[0].innerHTML;
+
+    if(currentState == 'Close'){
+        menu_toggle[0].innerHTML = 'Open';
+    }
+    else{
+        menu_toggle[0].innerHTML = 'Close';
+    }
+});
+
+
+
+animateButton.click(function() {
+    var currentState = animateButton[0].innerHTML;
+
+    if(currentState == 'Animate Off'){
+        animateaGARPH = true;
+        animateButton[0].innerHTML = 'Animate On';
+    }
+    else{
+        animateaGARPH = false;
+        animateButton[0].innerHTML = 'Animate Off';
+    }
+});
