09e3c10541e361cd1f981387cd094964c2e049f4
Jose Araujo <arjo1203@stcloudstate.edu>
2015-05-18 21:42:29 -0500

    adding functionality to video carousel 2.0

--- videoCarousel/videoCarousel.js
+++ videoCarousel/videoCarousel.js
@@ -1,29 +1,23 @@
-//use youtube api to create video
-//video should have a playlist...
-//youtube should be disabled so that user cant go to youtube
-//use pause and play buttons to allow user to still take control of a video
-
-//loads youtube api script
 var tag = document.createElement('script');
 tag.src = "https://www.youtube.com/iframe_api";
 
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-//create iframe and youtube player
-//playlist http://stackoverflow.com/questions/9313216/example-youtube-playlist-code
-var player;
+var player, videosDiv = document.getElementById('videos'), videos = $('#videos')[0];
+
+
+
+
 function onYouTubeIframeAPIReady(){
 
     player = new YT.Player('player', {
         height: '85%',
         width: '100%',
-        //playlistId: 'PL1DD10E84B9B08A35'
-        //videoId: '_nQDU7HOStc', //player.loadVideoById(videoId:String, startSeconds:Number, suggestedQuality:String):Void
         playerVars: {
             listType:'playlist',
             list: 'PL1DD10E84B9B08A35', //'PLwyv96l14uwm0HoKlOgfJ7vX7oJAEQssf' //'PL1DD10E84B9B08A35'
-            'controls': 0,
+            'controls': 1,
             'autohide': 1,
             'rel': 0
         },
@@ -34,50 +28,43 @@ function onYouTubeIframeAPIReady(){
     });
 }
 
+
+
+
 //API will call this function when player is ready
 function onPlayerReady(event){
     event.target.playVideo();
 }
 
+
+
+
+first = 0;
 //API calls when player state changes
-//playing video = state=1
-var done= false;
 function onPlayerStateChange(event){
-
-    if (event.data == YT.PlayerState.PLAYING && !done){
-        //setTimeout(stopVideo, 6000);
-        done=true;
-
-        //loads the videoplaylist sidebar elements
-        addElement();
-    }
-    //checkign that the data is passed
-    if(event.data == 1){
-        var playing = event.data;
-        console.log("is playing " + playing);
-
-    }
-    if(event.data == 2){
-        //trying to set data to an object to pass into a button
-        var paused = event.data;
-        console.log("is paused " + paused);
-
+    if (event.data == YT.PlayerState.PLAYING){
+        //loads the videoplaylist after the playlist has started playing
+        if(first == 0){
+            addElement();
+        }
+        first++;
     }
     if(event.data == 0){
         var ended = event.data;
-        console.log("vid ended " + ended);
+        changeVideo();
+        //console.log("vid ended " + ended);
+        //console.log(videosDiv);
     }
 
 }
 
-var buttonsDiv = document.getElementById('buttons');
+
 
 //creates the sidebar elements
-function addElement( ) {
-
-
+function addElement() {
     var playerList = player.getPlaylist();
-    buttonsDiv.style.width = (playerList.length * 150).toString() + 'px';
+    //console.log(playerList);
+    videosDiv.style.width = (playerList.length * 150).toString() + 'px';
 
 
     for (var j = 0; j < playerList.length; j++) {
@@ -89,9 +76,54 @@ function addElement( ) {
         input.style.width = '150px';
         input.style.height = '150px';
         input.onclick = function(){
+
+            for(var i = 0; i < videos.children.length; i++){
+                if(videos.children[i].id != this.id){
+                    $('#' + videos.children[i].id).removeClass('border');
+                }
+                else{
+                    $('#' + videos.children[i].id).addClass('border');
+                }
+            }
+
             player.loadVideoById(playerList[parseInt(this.id)]);
         };
 
-        buttonsDiv.appendChild(input);
+        if(j == 0){
+            //console.log(input);
+            input.className = 'border';
+        }
+
+        videosDiv.appendChild(input);
     }
+}
+
+
+
+function changeVideo(){
+    var videoIndex = findVideo(videos);
+
+    if(videoIndex == videos.children.length - 1){
+        var nextVideo = $('#0');
+    }
+    else{
+        var nextVideo = $('#' + videos.children[videoIndex + 1].id)[0];
+    }
+
+    nextVideo.click();
+}
+
+
+
+function findVideo(div){
+    for(var i = 0; i < div.children.length; i ++){
+        var video = div.children[i];
+
+        if(video.className == 'border'){
+            //console.log('found');
+            var index = i;
+        }
+    }
+
+    return index;
 }
