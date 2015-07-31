d2225c71a1c1cd7936d83052c2fb0dcbf276527d
Jose Araujo <arjo1203@stcloudstate.edu>
2015-07-31 10:36:32 -0500

    updating

--- multiDraw/multi-touch.js
+++ multiDraw/multi-touch.js
@@ -12,85 +12,53 @@ function startup(){
     
     var soilMap = new Raster('img');
     soilMap.position = paper.view.center;
-    //console.log(paper.view);
+    console.log(paper.view);
     
-    document.body.addEventListener('touchstart', function(e) {
-        touchStart(e);
-    }, false);
-    //document.body.addEventListener('touchmove', function(e) {
-    //    touchMove(e);
-    //}, false);
-    //document.body.addEventListener('touchend', function(e) {
-    //    touchEnd(e);
-    //}, false);
+    document.body.addEventListener('touchstart', touchStart, false);
+    document.body.addEventListener('touchmove', touchmove, false);
+    document.body.addEventListener('touchend', touchEnd, false);
 }
 
 
 
 //Array to store new paths
 var paths = [];
-var currentTouches = [];
 
 function touchStart(ev){
     ev.preventDefault();
     var touches = ev.touches;
 
     for(var i = 0; i < touches.length; i++) {
-        var touch = touches[i];
 
-        currentTouches.push({
-            id: touch.identifier,
-            pageX: touch.pageX,
-            pageY: touch.pageY,
-            color: '#3F3F3F',
-            path: new Path()
-        });
-
-        currentTouches[i].path.strokeColor = 'red';
-        currentTouches[i].path.strokeWidth = i * 4;
+        //Create new path per touch
+        var path = new Path();
+        path.strokeColor = 'red';
+        path.strokeColor = 'red';
+        path.strokeWidth = 4;
+        paths.push(path);
     }
 
-    console.log(currentTouches);
+    console.log(touches);
 
 }
 
 
-function touchMove(ev){
-    console.log(ev);
-    //ev.preventDefault();
-    //var touches = ev.changedTouches;
+function touchmove(ev){
+    ev.preventDefault();
+    var touches = ev.touches;
 
     //Prevents touch bubbling 
-    //if(touches.length == paths.length){
-    //    for(var i = 0; i < touches.length; i++){
-    //        var path = paths[i];
-    //        var touch = touches[i];
-    //        var x1, y1;
-    //        x1 = touch.pageX;
-    //        y1 = touch.pageY;
-    //        path.add(new Point(x1, y1));
-    //        paper.view.draw();
-    //    }
-    //}
-
-    //for(var i = 0; i < touches.length; i++) {
-    //    var touch = touches[i];
-    //    var currentTouchIndex = findCurrentTouchIndex(touch.identifier);
-    //
-    //    if(currentTouchIndex >= 0) {
-    //        var currentTouch = currentTouches[currentTouchIndex];
-    //        currentTouch.path.add(new Point({x: currentTouch.pageX, y: currentTouch.pageY}));
-    //
-    //        // Update the touch record.
-    //        currentTouch.pageX = touch.pageX;
-    //        currentTouch.pageY = touch.pageY;
-    //        // Store the record.
-    //        currentTouches.splice(currentTouchIndex, 1, currentTouch);
-    //    }
-    //    else {
-    //        console.log('Touch not found!');
-    //    }
-    //}
+    if(touches.length == paths.length){
+        for(var i = 0; i < touches.length; i++){
+            var path = paths[i];
+            var touch = touches[i];
+            var x1, y1;
+            x1 = touch.pageX;
+            y1 = touch.pageY;
+            path.add(new Point(x1, y1));
+            paper.view.draw();
+        }
+    }
 }
 
 
@@ -98,10 +66,10 @@ function touchEnd(ev){
     ev.preventDefault(); 
     var touches = ev.touches;
 
-    ////Empth paths array to start process over
-    //if(touches.length == 0){
-    //    paths.splice(0, paths.length);
-    //}
+    //Empth paths array to start process over
+    if(touches.length == 0){
+        paths.splice(0, paths.length);
+    }
 }
 
 
