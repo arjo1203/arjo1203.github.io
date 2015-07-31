52f412805c53f6fc7d72a607b1913172764f7bb9
Jose Araujo <arjo1203@stcloudstate.edu>
2015-07-31 10:09:13 -0500

    updating

--- multiDraw/multi-touch.js
+++ multiDraw/multi-touch.js
@@ -36,36 +36,49 @@ function touchStart(ev){
             id: touch.identifier,
             pageX: touch.pageX,
             pageY: touch.pageY,
-            color: '#3F3F3F'
+            color: '#3F3F3F',
+            path: new Path()
         });
 
 
         //Create new path per touch
-        var path = new Path();
-        path.strokeColor = 'red';
-        path.strokeWidth = 4;
-        paths.push(path);
+        //var path = new Path();
+        currentTouches[i].path.strokeColor = 'red';
+        currentTouches[i].path.strokeWidth = i * 4;
+        //paths.push(path);
     }
+
+    console.log(touches);
 
 }
 
 
 function touchmove(ev){  
     ev.preventDefault();
-    var touches = ev.touches;
+    var touches = ev.changedTouches;
 
     //Prevents touch bubbling 
-    if(touches.length == paths.length){
-        for(var i = 0; i < touches.length; i++){
-            var path = paths[i];
-            var touch = touches[i];
-            var x1, y1;
-            x1 = touch.pageX;
-            y1 = touch.pageY;
-            path.add(new Point(x1, y1));
-            paper.view.draw();  
+    //if(touches.length == paths.length){
+    //    for(var i = 0; i < touches.length; i++){
+    //        var path = paths[i];
+    //        var touch = touches[i];
+    //        var x1, y1;
+    //        x1 = touch.pageX;
+    //        y1 = touch.pageY;
+    //        path.add(new Point(x1, y1));
+    //        paper.view.draw();
+    //    }
+    //}
+
+    for(var i = 0; i < touches.length; i++) {
+        var touch = touches[i];
+        var currentTouchIndex = findCurrentTouchIndex(touch.identifier);
+
+        if(currentTouchIndex >= 0) {
+            var currentTouch = currentTouches[currentTouchIndex];
+            currentTouch.path.add(ev.point);
         }
-    } 
+    }
 }
 
 
@@ -73,10 +86,10 @@ function touchEnd(ev){
     ev.preventDefault(); 
     var touches = ev.touches;
 
-    //Empth paths array to start process over
-    if(touches.length == 0){
-        paths.splice(0, paths.length);
-    }
+    ////Empth paths array to start process over
+    //if(touches.length == 0){
+    //    paths.splice(0, paths.length);
+    //}
 }
 
 
