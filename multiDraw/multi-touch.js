7de5e5759533a3f965e331f322b34976706fd3dd
Jose Araujo <arjo1203@stcloudstate.edu>
2015-07-31 10:45:45 -0500

    reverting to old multi

--- multiDraw/multi-touch.js
+++ multiDraw/multi-touch.js
@@ -28,21 +28,15 @@ function touchStart(ev){
     ev.preventDefault();
     var touches = ev.touches;
 
-    //for(var i = 0; i < touches.length; i++) {
-
-        //Create new path per touch
-        var path = new Path();
-        path.strokeColor = 'red';
-        path.strokeWidth = 4;
-        paths.push(path);
-    //}
-
-    console.log(touches);
-
+    //Create new path per touch
+    var path = new Path();
+    path.strokeColor = 'red';
+    path.strokeWidth = 4;
+    paths.push(path);
 }
 
 
-function touchmove(ev){
+function touchmove(ev){  
     ev.preventDefault();
     var touches = ev.touches;
 
@@ -55,9 +49,9 @@ function touchmove(ev){
             x1 = touch.pageX;
             y1 = touch.pageY;
             path.add(new Point(x1, y1));
-            paper.view.draw();
+            paper.view.draw();  
         }
-    }
+    } 
 }
 
 
@@ -70,16 +64,3 @@ function touchEnd(ev){
         paths.splice(0, paths.length);
     }
 }
-
-
-// Finds the array index of a touch in the currentTouches array.
-var findCurrentTouchIndex = function (id) {
-    for (var i=0; i < currentTouches.length; i++) {
-        if (currentTouches[i].id === id) {
-            return i;
-        }
-    }
-
-    // Touch not found! Return -1.
-    return -1;
-};
