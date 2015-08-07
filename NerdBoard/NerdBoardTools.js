53cb40e529336b2b621f938b8c4b654c24413384
Jose Araujo <arjo1203@stcloudstate.edu>
2015-08-07 08:19:12 -0500

    updating moving tool

--- NerdBoard/NerdBoardTools.js
+++ NerdBoard/NerdBoardTools.js
@@ -321,16 +321,42 @@ NerdBoard.Tools = window.onload = (function() {
     wbTools.tools.erase.maxDistance = 3;
 
 
+    wbTools.tools.shape = new paper.Tool();
+    wbTools.tools.shape.onMouseDown = function(event) {
+        wbTools.drawShape(event.point, NerdBoard.shape, $('#textInput')[0].value);
+    };
+    wbTools.tools.shape.onMouseDrag = function(event) {
+        var last = paper.project.activeLayer.children[paper.project.activeLayer.children.length - 1];
+        last.position = event.point;
+    };
+    wbTools.tools.shape.onMouseUp = function(event) {
+        var state = $('#drawAfterCheckbox')[0].checked;
+
+        if(state) {
+            updateToDrawMode();
+        }
+    };
+    wbTools.tools.shape.minDistance = 1;
+    wbTools.tools.shape.maxDistance = 3;
+
+
     wbTools.tools.move = new paper.Tool();
     wbTools.tools.move.onMouseDown = function(paperEvent) {
         paperEvent.preventDefault();
 
         if(paperEvent.event.type == 'mousedown') {
-            var hitResult = project.hitTest(paperEvent.point, hitOptions);
-            if (hitResult) {
-                var hitItem = hitResult.item;
-                hitItem.data.touchId = 0;
-                console.log(hitItem);
+            var mouseHit = project.hitTest(paperEvent.point, hitOptions);
+            if (mouseHit) {
+                var mouseItem = mouseHit.item;
+                var mouseParent = mouseItem._parent;
+                var mouseParentName = mouseParent.name;
+
+                if(mouseParentName == 'layer1' && mouseItem.name !== 'bg') {
+                    mouseItem.data.touchId = 0;
+                }
+                else {
+                    mouseParent.data.touchId = 0;
+                }
             }
             else {
                 return ;
@@ -357,11 +383,14 @@ NerdBoard.Tools = window.onload = (function() {
                     currentTouches.push(trackedTouch);
 
                     var point = new Point({x: touch.pageX, y: touch.pageY});
-                    var hitResult = project.hitTest(point, hitOptions);
-                    if (hitResult) {
-                        var hitItem = hitResult.item;
-                        hitItem.data.touchId = touch.identifier;
-                        console.log(hitItem.data);
+                    var touchHit = project.hitTest(point, hitOptions);
+                    if (touchHit) {
+                        var touchItem = touchHit.item;
+                        var touchParent = touchItem._parent;
+                        var touchParentName = touchParent.name;
+
+                        touchItem.data.touchId = touch.identifier;
+                        console.log(touchParentName);
                     }
                     else {
                         return ;
@@ -376,25 +405,11 @@ NerdBoard.Tools = window.onload = (function() {
 
         if(paperEvent.event.type == 'mousemove') {
             var currentItemIndex = findItemInPaper(0);
-            console.log(currentItemIndex);
 
             if (currentItemIndex !== -1) {
                 var currentItem = paper.project.activeLayer.children[currentItemIndex];
-
-                var firstThree = currentItem.data.name[0] + currentItem.data.name[1]  + currentItem.data.name[2], id;
-                console.log(firstThree);
-
-                if (currentItem.data.name !== 'bg') {
-                    if (firstThree == 'rec' || firstThree == 'tex')  {
-                        id = currentItem.data.name.slice(4, currentItem.data.name.length);
-                        paper.project.activeLayer.children['group' + id].position.x += paperEvent.delta.x;
-                        paper.project.activeLayer.children['group' + id].position.y += paperEvent.delta.y;
-                    }
-                    else{
-                        currentItem.position.x += paperEvent.delta.x;
-                        currentItem.position.y += paperEvent.delta.y;
-                    }
-                }
+                    currentItem.position.x += paperEvent.delta.x;
+                    currentItem.position.y += paperEvent.delta.y;
             }
         }
 
